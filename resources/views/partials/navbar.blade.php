c7e836228bf928161731ceafb03fee5f0f9135d5
rizalkalam <rizalkalam78@gmail.com>
2023-01-21 21:35:39 +0700

    benerin navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -9,9 +9,9 @@
             <li class="nav-item">
               <a class="nav-link active fw-bold" style="color: #E0C28D;" aria-current="page" href="/beranda">Beranda</a>
             </li>
-            {{-- <li class="nav-item">
+            <li class="nav-item">
               <a class="nav-link " href="/produk.html">Produk</a>
-            </li> --}}
+            </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Kategori
