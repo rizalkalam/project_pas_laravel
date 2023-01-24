e13c78e7286fa34ed617b9992a0b026153efb573
rizalkalam <rizalkalam78@gmail.com>
2023-01-24 18:02:52 +0700

    nambahin fitur autentikasi dengan token

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\client\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// daftar dan masuk
+Route::post('/register', [\App\Http\Controllers\client\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\client\AuthController::class, 'login']);
+
+// setelah daftar dan masuk bisa akses ini
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
 });
